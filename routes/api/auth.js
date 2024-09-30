d56414a5a3c1b8cfb365e75c0856534fd9922f20
fullstack001 <mr.dimas332@gmail.com>
2024-09-30 21:19:25 +0300

    fix all

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -188,7 +188,7 @@ router.post("/signup-direct", async (req, res) => {
     });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server error");
+    res.status(500).send({ msg: "Server error" });
   }
 });
 
